a1f0eb1991de4647576dea1ad9c8a6267ab3801a
Aibal Jose <117338506+Aibaljose2004@users.noreply.github.com>
2022-12-10 12:28:12 +0530

    no error

---
+++ print_odd_numbers_in_descending_order_upto_limit.py
@@ -0,0 +1,12 @@
+#THIS IS CREATED BY AIBAL AND ANY ERROR IN THIS CODE IS NATURAL SO TAKE IT ON THAT SENSE.
+
+print("Program to print odd numbers in descending order upto limit\n ")
+i=0
+S=0
+n=int(input("Enter the limit: "))
+s=2*n-1
+
+while i<n:
+    i=i+1
+    print(s)
+    s=s-2
